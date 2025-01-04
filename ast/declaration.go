7b058d6a9142ad6bd08008e4e102a49ff066a908
Cylix Lee <cylix.lee@outlook.com>
2025-01-04 13:09:14 +0800

    Implemented all syntax (declarations, statements and expressions) AST in visitor pattern.

--- ast/declaration.go
+++ ast/declaration.go
@@ -1,1 +1,32 @@
 package ast
+
+type Declaration interface {
+	Accept(DeclarationVisitor)
+}
+
+type DeclarationVisitor interface {
+	VisitClass(*ClassDeclaration)
+	VisitFun(*FunDeclaration)
+	VisitVar(*VarDeclaration)
+}
+
+type ClassDeclaration struct {
+	Name      Identifier
+	Baseclass *Identifier
+	Methods   []FunDeclaration
+}
+
+type FunDeclaration struct {
+	Name       Identifier
+	Parameters []Identifier
+	Body       *BlockStatement
+}
+
+type VarDeclaration struct {
+	Name        Identifier
+	Initializer Expression
+}
+
+func (c *ClassDeclaration) Accept(visitor DeclarationVisitor) { visitor.VisitClass(c) }
+func (f *FunDeclaration) Accept(visitor DeclarationVisitor)   { visitor.VisitFun(f) }
+func (v *VarDeclaration) Accept(visitor DeclarationVisitor)   { visitor.VisitVar(v) }
--- ast/statement.go
+++ ast/statement.go
@@ -1,1 +1,58 @@
 package ast
+
+type Statement interface {
+	Accept(StatementVisitor)
+}
+
+type StatementVisitor interface {
+	VisitExpressionStatement(*ExpressionStatement)
+	VisitFor(*ForStatement)
+	VisitIf(*IfStatement)
+	VisitPrint(*PrintStatement)
+	VisitReturn(*ReturnStatement)
+	VisitWhile(*WhileStatement)
+	VisitBlock(*BlockStatement)
+}
+
+type ExpressionStatement struct {
+	Expression Expression
+}
+
+type ForStatement struct {
+	ExpressionInitializer Expression
+	VarInitializer        *VarDeclaration
+	Condition             Expression
+	Increment             Expression
+	Body                  Statement
+}
+
+type IfStatement struct {
+	Condition Expression
+	Then      Statement
+	Otherwise Statement
+}
+
+type PrintStatement struct {
+	Expression Expression
+}
+
+type ReturnStatement struct {
+	Expression Expression
+}
+
+type WhileStatement struct {
+	Condition Expression
+	Body      Statement
+}
+
+type BlockStatement struct {
+	Declarations []Declaration
+}
+
+func (es *ExpressionStatement) Accept(visitor StatementVisitor) { visitor.VisitExpressionStatement(es) }
+func (f *ForStatement) Accept(visitor StatementVisitor)         { visitor.VisitFor(f) }
+func (i *IfStatement) Accept(visitor StatementVisitor)          { visitor.VisitIf(i) }
+func (p *PrintStatement) Accept(visitor StatementVisitor)       { visitor.VisitPrint(p) }
+func (r *ReturnStatement) Accept(visitor StatementVisitor)      { visitor.VisitReturn(r) }
+func (w *WhileStatement) Accept(visitor StatementVisitor)       { visitor.VisitWhile(w) }
+func (b *BlockStatement) Accept(visitor StatementVisitor)       { visitor.VisitBlock(b) }
